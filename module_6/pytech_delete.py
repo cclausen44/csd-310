a5b73c2284f2ae7a6c060512405e5b0951dd7788
Chris Clausen <clausenchristopher44@gmail.com>
2022-06-21 10:58:32 -0400

    New Commit

---
+++ module_6/pytech_delete.py
@@ -0,0 +1,56 @@
+# Christopher Clausen
+# 6/21/2022
+# Module 6.3 Assignment
+# 
+
+# import MongoClient
+from pymongo import MongoClient
+# Create a variable, url, 
+url ="mongodb+srv://admin:admin@cluster0.o3uwqap.mongodb.net/?retryWrites=true&w=majority"
+# Create a variable named client
+client = MongoClient(url)
+# Create a variable named db
+db = client["pytech"]
+# Create a collection
+pytech = db["students"]
+
+# Use the find() method to display all documents in the collection
+print("- - DISPLAYING STUDENT DOCUMENTS FROM find() QUERY - -")
+docs = db.students.find({})
+for doc in docs:
+    print(f"Student ID: {doc['student_id']}\nFirst Name: {doc['first_name']}\nLast Name: {doc['last_name']}\n")
+
+# Create another document
+ambriell = {
+    "student_id": "1010",
+    "first_name": "Ambriell",
+    "last_name": "Hier"
+}
+
+students = [ambriell]
+
+print("- - INSERT STATEMENTS - -")
+# Use the insert() method to add another document
+for student in students:
+    student_id = pytech.insert_one(student).inserted_id
+    print(f"Inserted student record {student['first_name']} {student['last_name']} into the students collection with document_id {student_id}")
+
+print("\n")
+
+# Display the new list of student documents
+print("- - DISPLAYING NEW STUDENT LIST DOC - -")
+docs = db.students.find({})
+for doc in docs:
+    print(f"Student ID: {doc['student_id']}\nFirst Name: {doc['first_name']}\nLast Name: {doc['last_name']}\n")
+
+# Call the delete_one() method to delete latest student document
+myQuery ={"student_id": "1010"}
+pytech.delete_one(myQuery)
+
+# Display the updated list of student documents
+print("- - DELETED STUDENT ID: 1010 - -")
+docs = db.students.find({})
+for doc in docs:
+    print(f"Student ID: {doc['student_id']}\nFirst Name: {doc['first_name']}\nLast Name: {doc['last_name']}\n")
+
+print("\n\nEnd of program, press any key to exit...")
---
+++ module_6/pytech_update.py
@@ -0,0 +1,49 @@
+# Christopher Clausen
+# 6/21/2022
+# Module 6.2 Assignment
+# 
+
+# import MongoClient
+from pymongo import MongoClient
+# Create a variable, url, 
+url ="mongodb+srv://admin:admin@cluster0.o3uwqap.mongodb.net/?retryWrites=true&w=majority"
+# Create a variable named client
+client = MongoClient(url)
+# Create a variable named db
+db = client["pytech"]
+# Create a collection
+pytech = db["students"]
+
+# Create the documents for each student
+chris = {
+    "student_id": "1007", 
+    "first_name": "Chris", 
+    "last_name": "Clausen"
+    }
+trevor = {
+    "student_id": "1008", 
+    "first_name": "Trevor", 
+    "last_name": "Bailey"
+    }
+taylor = {
+    "student_id": "1009", 
+    "first_name": "Taylor", 
+    "last_name": "Pallete"
+    }
+
+# Use the find() method to display all documents in the collection
+print("- - DISPLAYING STUDENT DOCUMENTS FROM find() QUERY - -")
+docs = db.students.find({})
+for doc in docs:
+    print(f"Student ID: {doc['student_id']}\nFirst Name: {doc['first_name']}\nLast Name: {doc['last_name']}\n")
+
+# Use the update method to change the last name of a document.
+result = pytech.update_one(
+    {"student_id": "1007"}, {"$set": {"last_name": "Bailey"}})
+
+# Use the find_one() method to display 1 document
+print("\n- - DISPLAYING UPDATED STUDENT DOCUMENT FROM find_one() QUERY - -")
+clausen = pytech.find_one({"student_id": "1007"})
+print(f"Student ID: {clausen['student_id']}\nFirst Name: {clausen['first_name']}\nLast Name: {clausen['last_name']}")
+
+print("\n\nEnd of program, press any key to exit...")
