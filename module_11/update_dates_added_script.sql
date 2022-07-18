510bbe3c3a0777548186ea9ebe8a033aa34cf776
Chris Clausen <clausenchristopher44@gmail.com>
2022-07-17 22:11:13 -0400

    7/17/2022

---
+++ module_11/update_dates_added_script.sql
@@ -0,0 +1,24 @@
+-- update dates added in clients
+UPDATE CLIENT
+SET cl_date_added = '2021-05-23'
+WHERE cl_id = 1;
+
+UPDATE CLIENT
+SET cl_date_added = '2022-03-12'
+WHERE cl_id = 2;
+
+UPDATE CLIENT
+SET cl_date_added = '2022-02-22'
+WHERE cl_id = 3;
+
+UPDATE CLIENT
+SET cl_date_added = '2021-12-23'
+WHERE cl_id = 4;
+
+UPDATE CLIENT
+SET cl_date_added = '2022-04-01'
+WHERE cl_id = 5;
+
+UPDATE CLIENT
+SET cl_date_added = '2022-05-17'
+WHERE cl_id = 6;
